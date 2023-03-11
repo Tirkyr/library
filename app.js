a1a3984d16225115152ecb1d6b35d7309f147a34
Vincenzo <vincenzo.delluniversit@virgilio.it>
2023-03-11 12:48:56 +0100

    Routing

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
 
-const port = process.env.PORT || 3000;
-
 const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
@@ -18,8 +17,19 @@ app.use('/js', express.static(path.join(__dirname, 'node_modules/jquery/dist')))
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
+const nav = [
+  { link: '/books', title: 'Books' },
+  { link: '/authors', title: 'Author' }
+];
+
+const bookRouter = require('./src/routes/bookRoutes')(nav);
+
+app.use('/books', bookRouter);
 app.get('/', (req, res) => {
-  res.render('index', { list: ['a', 'b'], title: 'Library' });
+  res.render('index', {
+    nav,
+    title: 'Library'
+  });
 });
 
 app.listen(port, () => {
